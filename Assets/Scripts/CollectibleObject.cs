a062c1f2726932a05a74eb14e643e70371a17f28
kir-55 <56845325+kir-55@users.noreply.github.com>
2023-06-21 04:58:08 +0200

    Update CollectibleObject.cs
    - deleted isCollectable and Collect method

--- Assets/Scripts/CollectibleObject.cs
+++ Assets/Scripts/CollectibleObject.cs
@@ -4,22 +4,14 @@ using UnityEngine;
 
 public class CollectibleObject : MonoBehaviour
 {
-    // Define a class to hold information about each collectible type
-    public bool IsCollectable = true;
     public int ItemTupe;
     public int itemAmount = 1;
     public float InstantiationTime;
 
-    // Method to collect the object and apply its effect
     private void Start()
     {
         InstantiationTime = Time.time;
-    }
-    public void Collect()
-    {
-        if(IsCollectable)
-            Destroy(gameObject);
-    }
+    
 
     private void OnTriggerStay2D(Collider2D other)
     {
